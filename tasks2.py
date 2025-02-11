ff6b9ccb35c29dc4ed478c9a768eb714bc031666
Edgar Lipnevic <edgarlipnevic@gmail.com>
2025-02-11 18:31:08 +0200

    4-5 tasks add

--- tasks2.py
+++ tasks2.py
@@ -35,4 +35,68 @@ print(f'Atlyginimas: {atl2}')
 print(darb1.lytis)
 atlmk = darb1.mokesciai
 print(f'Po mokesciu: {atlmk}')
+#  Task 4
+class Matematika:
+    @staticmethod
+    def add(num1, num2):
+        return num1 + num2
+
+    @staticmethod
+    def minus(num1, num2):
+        return num1 - num2
+
+    @staticmethod
+    def multiply(num1, num2):
+        return num1 * num2
+
+    @staticmethod
+    def divide(num1, num2):
+        return num1 / num2
+
+    @staticmethod
+    def ar_lyginis(num3):
+        return num3 % 2 ==0
+a = Matematika.add(1,2)
+print(a)
+a = Matematika.minus(2,1)
+print(a)
+a = Matematika.multiply(2,2)
+print(a)
+a = Matematika.divide(6,2)
+print(a)
+a = Matematika.ar_lyginis(7)
+print(a)
+print('-'*40)
+# Task 5
+class Automobilis:
+    def __init__(self, marke, modelis, metai):
+        self.marke = marke
+        self.modelis = modelis
+        self.metai = metai
+
+    @classmethod
+    def sukurti_is_string(cls, eilute):
+        marke, modelis, metai = eilute.split()
+        return cls(marke, modelis, metai)
+
+    def __str__(self):
+        return f'{self.marke} {self.modelis} {self.metai}'
+
+    @classmethod
+    def naujausias_modelis(cls, automobiliai):
+        return max(automobiliai, key=lambda listas: listas.metai)
+
+auto1 = Automobilis.sukurti_is_string('BMW XM 2024')
+auto2 = Automobilis.sukurti_is_string('Audi RS6 2025')
+auto3 = Automobilis.sukurti_is_string('Mercedes S63 2023')
+automobiliai = [auto1, auto2, auto3]
+
+print(auto1)
+print(auto2)
+print(auto3)
+naujausias = Automobilis.naujausias_modelis(automobiliai)
+print(f'Naujausias: {naujausias}')
+
+
+
 
